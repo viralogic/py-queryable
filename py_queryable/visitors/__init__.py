176c6c2a439d1f871e23da7f030aaab8e4804d9e
viralogic <bwfenske@ualberta.ca>
2018-01-30 00:33:30 -0700

    * where queryable function with testing * removed IExpressionVisitor interface

--- py_queryable/visitors/__init__.py
+++ py_queryable/visitors/__init__.py
@@ -1,39 +1,8 @@
-import abc
-
-
-class IExpressionVisitor(object):
-
+class Visitor(object):
     def visit(self, expression):
         name = expression.__class__.__name__
         return getattr(self, u"visit_{0}".format(name))(expression)
 
-    @abc.abstractmethod
-    def visit_UnaryExpression(self, expression):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def visit_SelectExpression(self, expression):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def visit_TableExpression(self, expression):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def visit_WhereExpression(self, expression):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def visit_CountExpression(self, expression):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def visit_TakeExpression(self, expression):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def visit_SkipExpression(self, expression):
-        raise NotImplementedError()
 
 
 
